4eed0f3d84daa7fd3187b16b060e862f6df8dd20
Victor Santos Barreto <denvictor.sbarreto@gmail.com>
2023-11-10 23:24:58 -0300

    first commit

---
+++ Nivel01/ex001.js
@@ -0,0 +1,15 @@
+// 1. Declare uma variável chamada "nome" e atribua seu nome a ela.
+// 2. Declare uma variável chamada "idade" e atribua sua idade a ela.
+// 3. Declare uma variável chamada "ehEstudante" e atribua um valor booleano representando se você é estudante ou não.
+
+// 1
+var nome = "Victor";
+console.log(none);
+
+// 2
+var idade = 10;
+console.log(idade);
+
+// 3
+var ehEstudante = true;
+console.log(ehEstudante);
