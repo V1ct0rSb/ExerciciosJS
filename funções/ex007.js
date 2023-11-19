3d93c4e7f122168c63288f7e2439db9232678095
Victor Santos Barreto <denvictor.sbarreto@gmail.com>
2023-11-19 19:09:54 -0300

    Exercicios

--- funções/ex007.js
+++ funções/ex007.js
@@ -1,1 +1,12 @@
-// Exercício 4: Declare duas variáveis booleanas e use operadores lógicos para imprimir o resultado das seguintes operações: AND, OR e NOT
+// Sistema de Autenticação:
+// Crie um sistema de autenticação simples. Solicite ao usuário que insira um nome de usuário e uma senha. Se o nome de usuário for "admin" e a senha for "12345", imprima "Login bem-sucedido". Caso contrário, informe "Credenciais incorretas".
+
+var senha = 12345;
+var usuario = "admin";
+
+var status =
+  senha === 12345 && usuario === "admin"
+    ? "Seja Bem-vindo"
+    : "Credenciais Incorretas";
+
+console.log(status);
